6a95c57278aaf5f3fd682e4f4a218fa91aa9258d
ajarn <ajarn>
2016-12-19 21:43:47 +0100

    Test funkcjonalny teraz sprawdza mozliwosc dodania elementu do listy

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -6,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser=webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         #Edyta dowiedziala sie o nowej aplikacji "lista rzeczy do zrobienia"
         #Postanowila wiec przejsc na strone glowna aplikacji
         self.browser.get('http://localhost:8000')
@@ -14,17 +15,31 @@ class NewVisitorTest(unittest.TestCase):
         
         #Zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy
         self.assertIn('Listy', self.browser.title)
-        self.fail('Zakonczenie testu!')
-
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy',header_text)
 
         #Od razu zostaje zachecona aby wpisac rzecz do zrobienia
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Wpisz rzecz do zrobienia')
+
         #W polu tekstowym wpisala "Kupic pawie piora (hobby Edyty polega na tworzeniu ozdobnych przynet)
+        inputbox.send_keys('Kupic pawie piora')
+
         #Po nacisnieciu Enter strona zostala zaktualizowana i wyswietla
         #"1:Kupic pawie piora" jako element listy rzeczy do zrobienia
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1:Kupic pawie piora' for row in rows))
 
         #Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnej rzeczy do zrobienia
         #Edyta wpisala "Uzyc pawich pior do zrobienia przynety" (Edyta jest niezwykle skrupulatna)
 
+        self.fail('Zakonczenie testu!')
+
+
+        
         #Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia
 
         #Edyta byla ciekawa czy witryna zapamieta jej liste. Zwrocila uwage na wygenerowany dla niej unikatowy adres URL, obok ktorego znajduje sie pewien tekst 

069cc918516637ff3065709095116ce71c680213
ajarn <ajarn>
2017-01-07 21:13:14 +0100

    Dodanie widoku new_list

--- lists/tests.py
+++ lists/tests.py
@@ -39,34 +39,24 @@ class HomePageTest(TestCase):
         expected_html=render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request=HttpRequest()
-        request.method='POST'
-        request.POST['item_text'] = 'Nowy element listy'
-        response = home_page(request)
+    def test_saving_a_POST_request(self):
         
+        self.client.post('/lists/new',data = {'item_text': 'Nowy element listy'})
         self.assertEqual(Item.objects.count(),1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'Nowy element listy')
     
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text']  = 'Nowy element listy'
+    def test_redirects_after_POST(self):
+        
 
-        response = home_page(request)
+        response = self.client.post('/lists/new', data = {'item_text': 'Nowy element listy'})
 
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
         
 #        self.assertIn('Nowy element listy',response.content.decode())
 #        expected_html = render_to_string('home.html',{'new_item_text': 'Nowy element listy'})
 #        self.assertEqual(response.content.decode(),expected_html)
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(),0)
 
 
 class ListViewTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -5,17 +5,17 @@ from lists.models import Item
 
 def home_page(request):
     #return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
-    
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request,'home.html')
 
 def view_list(request):
     
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
+
+def new_list(request):
+    
+    Item.objects.create(text = request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 
 # Create your views here.

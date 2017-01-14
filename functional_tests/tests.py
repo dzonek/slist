af09fa5af4d882ad521c9827fa7c891cfc7c484c
ajarn <ajarn>
2017-01-14 21:19:43 +0100

    Uzycie Bootstrap do poprawy ukladu graficznego.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
     def tearDown(self):
@@ -89,5 +89,13 @@ class NewVisitorTest(LiveServerTestCase):
         #Usatysfakcjonowani klada sie spac
         self.fail('Zakonczenie testu!')
 
+    def test_layout_and_styling(self):
+        #Edyta przeszla na strone glowna
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #Zauwazyla, ze pole tekstowe zostalo elegancko wysrodkowane
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta=5)
 #if __name__=='__main__':
 #    unittest.main()#warnings='ignore')
---
+++ lists/urls.py
@@ -0,0 +1,29 @@
+"""superlists URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+#OD django 1.10 trzeba importowac w urls konkretne funkcje!
+from lists.views import home_page
+from lists.views import view_list,new_list,add_item
+
+urlpatterns = [
+#        url(r'^$',home_page, name='home'),
+        url(r'^lists/(\d+)/$',view_list, name = 'view_list'),
+        url(r'^lists/(\d+)/add_item$', add_item, name = 'add_item'),
+        #    url(r'^admin/', admin.site.urls),
+        url(r'^lists/new$',new_list,name='new_list'),
+]
